75fd5c1bc6cea14368c76a6592b17d59b1031a71
ahmad-tunggi <ahmadtunggi14@gmail.com>
2023-08-15 13:52:34 -0700

    fix ini

--- app/src/main/java/com/example/apk/fragment/TambahpertanyaanFragment.java
+++ app/src/main/java/com/example/apk/fragment/TambahpertanyaanFragment.java
@@ -1,16 +1,19 @@
 package com.example.apk.fragment;
 
 import android.os.Bundle;
-
-import androidx.appcompat.widget.AppCompatButton;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.apk.R;
 import com.example.apk.api.Services;
@@ -24,20 +27,22 @@ public class TambahpertanyaanFragment extends Fragment {
     FragmentManager fragmentManager;
     AppCompatButton btn_cancel, btn_simpan;
     EditText edit_tambah;
+    Toolbar toolbar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_tambahpertanyaan, container, false);
 
         btn_cancel = v.findViewById(R.id.btn_cancel);
         edit_tambah = v.findViewById(R.id.edit_tambah);
         btn_simpan = v.findViewById(R.id.btn_simpan);
+        toolbar = v.findViewById(R.id.toolbar);
+
         btn_cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentManager = requireActivity().getSupportFragmentManager();
                 fragmentManager.beginTransaction()
                         .replace(R.id.frame_layout, new DaftarpengajuanFragment())
                         .commit();
@@ -49,36 +54,40 @@ public class TambahpertanyaanFragment extends Fragment {
             public void onClick(View v) {
                 String sText = edit_tambah.getText().toString();
                 String kd_surat = getArguments().getString("kd_surat");
-//                Toast.makeText(getActivity(), kd_surat, Toast.LENGTH_SHORT).show();
-                if (sText.equals("")){
+
+                if (sText.isEmpty()){
                     edit_tambah.setError("Harus diisi!");
-                }else {
+                } else {
                     postData(kd_surat, sText);
+                    Toast.makeText(getContext(), "Tambah Pertanyaan Berhasil", Toast.LENGTH_SHORT).show();
+
+
                 }
             }
         });
-        return  v;
+
+        return v;
     }
 
     private void postData(String kd_surat, String sText) {
         ApiRequest apiRequest = Services.koneksi().create(ApiRequest.class);
         Call call = apiRequest.tambahPertanyaan(kd_surat, sText);
-        call.enqueue(new Callback() {
+        call.enqueue(new Callback<Void>() {
             @Override
-            public void onResponse(Call call, Response response) {
+            public void onResponse(Call<Void> call, Response<Void> response) {
                 if (response.isSuccessful()){
-                    fragmentManager = getActivity().getSupportFragmentManager();
+                    fragmentManager = requireActivity().getSupportFragmentManager();
                     fragmentManager.beginTransaction()
                             .replace(R.id.frame_layout, new DaftarpengajuanFragment())
                             .commit();
-                }else {
+                } else {
                     Toast.makeText(getContext(), "Tambah Pertanyaan Gagal", Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
-            public void onFailure(Call call, Throwable t) {
-//                    Toast.makeText(getActivity(), t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            public void onFailure(Call<Void> call, Throwable t) {
+                // Handle failure here
             }
         });
     }
